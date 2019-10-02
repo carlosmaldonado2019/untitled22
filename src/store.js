4ff8e2b43e0f98c7776d318c11ae1499e7625a20
Carlos Maldonado <camv29@gmail.com>
2019-10-01 19:13:59 -0700

    asd

--- src/store.js
+++ src/store.js
@@ -5,10 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    frutas:[
+      {nombre: 'Manzana',cantidad:0},
+      {nombre: 'Pera',cantidad:0},
+      {nombre: 'Naranja',cantidad:0}
+    ]
 
   },
   mutations: {
-
+    aumentar(state,index){
+      state.frutas[index].cantidad++;
+    },
+    reiniciar(state){
+     // for (item of state.frutas){
+       // item.cantidad = 0;
+      //}
+      state.frutas.forEach(elemento =>{
+        elemento.cantidad= 0;
+      })
+    }
   },
   actions: {
 
